ba15dc729768f7f5f0ff616bce3c3d848afc854e
Ben Bradley <benbradley@users.noreply.github.com>
2017-05-18 10:45:22 +0100

    hostnamerandomizer Switch from using pwgen to things that are more likely to be installed. shuf is in coreutils so shd be ok

--- hostnamerandomizer.sh
+++ hostnamerandomizer.sh
@@ -2,13 +2,16 @@
 
 # Set a random hostname between 3-9 chars.
 # Ben Bradley 2016. https://github.com/benbradley
+# Using hostnamectl needs further systemd fudging - doing it the old way for now
+
 
 # GET A RANDOM LENGTH
+#RANDLEN=$(grep -m1 -ao '[3-9]' /dev/urandom | sed s/0/10/ | head -n1)
 RANDLEN=$(shuf -i 3-9 -n 1)
 
 # GET RANDOM STRING
-#RANDHOST=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $RANDLEN | head -n1)
-RANDHOST=$(pwgen $RANDLEN 1)
+#RANDHOST=$(pwgen $RANDLEN 1)
+RANDHOST=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w "$RANDLEN" | head -n1)
 
 # GET CURRENT HOSTNAME
 CURHOST=$(hostname -f)
